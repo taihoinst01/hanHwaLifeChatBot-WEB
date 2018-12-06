1ef7619a8d4b0d832990c8a297bdc70315b76ce5
taihoinst01 <gjlim@taihoinst.com>
2018-12-06 15:52:33 +0900

    Update botchat.js

--- webapps/ROOT/assets/js/botchat.js
+++ webapps/ROOT/assets/js/botchat.js
@@ -13380,7 +13380,7 @@
                         }
                     }
                 this._element.innerHTML = this._element.innerHTML.replace("/n", "</br>");
-                this._element.innerHTML = this._element.innerHTML.replace("/t", "&nbsp;");
+                //this._element.innerHTML = this._element.innerHTML.replace("/t", "&nbsp;");
                 
                 return this._element
             }, e.prototype.getBackgroundColor = function() {
